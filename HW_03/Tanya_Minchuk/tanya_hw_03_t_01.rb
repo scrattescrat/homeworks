cfc3410da9b5afabb662cd890c543fbb49b1a1bc
Tanya Minchuk <zhoralux@gmail.com>
2019-04-18 15:50:12 +0300

    Add homework 04

--- HW_03/Tanya_Minchuk/tanya_hw_03_t_01.rb
+++
@@ -1,9 +0,0 @@
-def task_1(text)
-  strings = text.split("\n")
-  res = ''
-
-  strings.each do |st|
-    res = st if st.downcase.include?('error')
-  end
-  res
-end
--- HW_03/Tanya_Minchuk/tanya_hw_03_t_04.rb
+++
@@ -1,5 +0,0 @@
-def task_4(text)
-  text2 = text.chars
-  res = text2.inject(0) { |sum, n| sum + n.to_i }
-  res
-end
