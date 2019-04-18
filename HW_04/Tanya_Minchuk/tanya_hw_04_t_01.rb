3b53688bfb0f7199c396dc074dc109b52d4240d8
Tanya Minchuk <zhoralux@gmail.com>
2019-04-18 19:34:28 +0300

    correct task 1

--- HW_04/Tanya_Minchuk/tanya_hw_04_t_01.rb
+++ HW_04/Tanya_Minchuk/tanya_hw_04_t_01.rb
@@ -1,10 +1,12 @@
 def task_4_1(num)
-  return '' if num > 1000 || num < 0
-  return '0' if num.zero?
+  return [] if num > 1000 || num < 0
+  return [0] if num.zero?
+  return [1] if num == 1
 
   fib = [1, 1]
   (num - 2).times do
     fib << fib[-1] + fib[-2]
   end
-  puts fib.join(' ')
+  fib
 end
+puts task_4_1(1)
--- HW_04/Tanya_Minchuk/tanya_hw_04_t_03.rb
+++ HW_04/Tanya_Minchuk/tanya_hw_04_t_03.rb
@@ -1,4 +1,3 @@
 def task_4_3(input)
   sorted_arr = input.flatten.uniq.sort { |a, b| b <=> a }
-  puts sorted_arr
 end
