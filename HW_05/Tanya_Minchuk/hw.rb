a64ff67312371417ba26b591b69982c8a351c7db
scrattescrat <48800974+scrattescrat@users.noreply.github.com>
2019-05-27 12:10:50 +0300

    Delete hw.rb

--- HW_05/Tanya_Minchuk/hw.rb
+++
@@ -1,9 +0,0 @@
-# Class Homework
-class Homework
-  attr_accessor :hw_title, :hw_solution
-
-  def initialize(options = {})
-    @hw_title     = options[:hw_title]
-    @hw_solution  = options[:hw_solution]
-  end
-end
